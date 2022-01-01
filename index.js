e3864eefc38d114d30f9a2ab165ff472d6f91779
avc0021 <avc0021@gmail.com>
2022-01-01 16:54:26 -0600

    finish questions and add text file for license

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const inquirer = require('inquirer');
 const questions = () => {
     return inquirer.prompt([
         {
-        type: 'input',
-        name: 'name',
-        message: 'What is the name of your project? (Required)',
-        validate: nameInput => {
-            if (nameInput) {
+          type: 'input',
+          name: 'projectName',
+          message: 'What is the name of your project? (Required)',
+          validate: projectName => {
+            if (projectName) {
               return true;
             } else {
             console.log('You need to enter a project name!');
@@ -19,19 +19,88 @@ const questions = () => {
           }
         },
         {
-            type: 'input',
-            name: 'name',
-            message: 'Who contributed to the project? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter credits.');
-                    return false;
-                }
+          type: 'input',
+          name: 'name',
+          message: 'Who contributed to the project? (Required)',
+          validate: nameInput => {
+            if (nameInput) {
+              return true;
+            } else {
+            console.log('Please enter credits.');
+              return false;
             }
+          }
         },
-
+        {
+          type: 'input',
+          name: 'github',
+          message: 'Enter your GitHub Username (Required)',
+          validate: githubInput => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log('Please enter your GitHub username!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project (Required)',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project description!');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you this project with? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+          type: 'input',
+          name: 'install',
+          message: 'Provide the required steps needed to install and run application. (Required)',
+          validate: installInput => {
+            if (installInput) {
+              return true;
+            } else {
+              console.log('Please provide install instructions to proceed.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'input',
+          name: 'useage',
+          message: 'Describe intended use of your project.',
+          validate: descriptionInput => {
+            if (descriptionInput) {
+              return true;
+            } else {
+              console.log('Enter description to proceed.');
+              return false;
+            }
+          }
+        },
+        {
+          type: 'confirm',
+          name: 'confirmLicense',
+          message: 'Would you like to enter a open source license?',
+          default: true
+        },
+        {
+          type: 'input',
+          name: 'license',
+          message: 'Provide test(s) for application.',
+          default: true
+        }
 
   ])
 };
